ec28bc105d496dd36a22f3154fbae7080ed8bbfb
Heru Azy <heruazyp@gmail.com>
2023-11-14 23:15:51 +0700

    FIX: Perbaikan format document menggunakan prettier

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 // TOGGLE NAVBAR
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector('.navbar-nav')
 
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active')
+}
 
 // CLICK OUTSIDE NAVBAR
-const hamburger = document.querySelector("#hamburger-menu");
+const hamburger = document.querySelector('#hamburger-menu')
 
-document.addEventListener("click", function (e) {
+document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+    navbarNav.classList.remove('active')
   }
-});
+})
