9b5a053cfc102e416a502d0a45c188f6cd617fac
Heru Azy <heruazyp@gmail.com>
2023-11-15 00:35:47 +0700

    Add Feature: Search box

--- src/script.js
+++ src/script.js
@@ -5,11 +5,26 @@ document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active')
 }
 
-// CLICK OUTSIDE NAVBAR
+// TOGGLE SEARCH
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-btn').onclick = (e) => {
+  searchForm.classList.toggle('active')
+  searchBox.focus()
+  e.preventDefault()
+}
+
+// CLICK OUTSIDE ELEMENT
 const hamburger = document.querySelector('#hamburger-menu')
+const searchBtn = document.querySelector('#search-btn')
 
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active')
+  }
+
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active')
   }
 })
